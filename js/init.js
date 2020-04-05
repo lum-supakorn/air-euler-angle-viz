22fb49a380fbf17d4c444136238de67dea2e3e0d
Lum <supakorn.s@protonmail.com>
2020-04-05 22:36:46 +0700

    Change ArrowHelper to AxesHelper

--- js/init.js
+++ js/init.js
@@ -19,12 +19,5 @@ grid_helper.rotateX(Math.PI / 2); // NED
 scene.add( grid_helper );
 
 // Set up arrow helper
-var x_dir = new THREE.Vector3( 10, 0, 0 );
-var y_dir = new THREE.Vector3( 0, 10, 10 );
-var z_dir = new THREE.Vector3( 0, 0, 10 );
-var origin = new THREE.Vector3( 0, 0, 0 );
-var length = 15;
-var x_arrow = new THREE.ArrowHelper( x_dir, origin, length, 0xFF0000 );
-var y_arrow = new THREE.ArrowHelper( y_dir, origin, length, 0x00FF00 );
-var z_arrow = new THREE.ArrowHelper( z_dir, origin, length, 0x0000FF );
-scene.add( x_arrow, y_arrow, z_arrow );
+var axesHelper = new THREE.AxesHelper(10);
+scene.add( axesHelper );

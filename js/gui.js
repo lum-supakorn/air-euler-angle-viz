bbf7f7f44272fd94665685ad87d3862219009b51
Lum <supakorn.s@protonmail.com>
2020-04-06 00:27:58 +0700

    Add all reset buttons

--- js/gui.js
+++ js/gui.js
@@ -13,9 +13,19 @@ var options = {
         phid: 0,
         thetad: 0,
         psid: 0,
+        resetAttRate: function() {
+            this.phid = 0;
+            this.thetad = 0;
+            this.psid = 0;
+        },
         p: 0,
         q: 0,
         r: 0,
+        resetBodyRate: function() {
+            this.p = 0;
+            this.q = 0;
+            this.r = 0;
+        },
     }
 };
 
@@ -28,14 +38,16 @@ var linearProp = gui.addFolder('Linear');
 linearProp.add(options.linear, 'x', -50, 50).name('\\(x\\)').listen();
 linearProp.add(options.linear, 'y', -50, 50).name('\\(y\\)').listen();
 linearProp.add(options.linear, 'z', -50, 50).name('\\(z\\)').listen();
-linearProp.add(options.linear, 'resetPos').name('reset position');
+linearProp.add(options.linear, 'resetPos').name('Reset position');
 linearProp.open();
 // Angular velocity
 var angularProp = gui.addFolder('Angular');
 angularProp.add(options.angular, 'phid', -10, 10, 0.5).name('\\(\\dot{\\phi}\\)').listen();
 angularProp.add(options.angular, 'thetad', -10, 10, 0.5).name('\\(\\dot{\\theta}\\)').listen();
 angularProp.add(options.angular, 'psid', -10, 10, 0.5).name('\\(\\dot{\\psi}\\)').listen();
+angularProp.add(options.angular, 'resetAttRate').name('Reset attitude rate');
 angularProp.add(options.angular, 'p', -10, 10, 0.5).name('\\(p\\)').listen();
 angularProp.add(options.angular, 'q', -10, 10, 0.5).name('\\(q\\)').listen();
 angularProp.add(options.angular, 'r', -10, 10, 0.5).name('\\(r\\)').listen();
+angularProp.add(options.angular, 'resetBodyRate').name('Reset body rate');
 angularProp.open();
